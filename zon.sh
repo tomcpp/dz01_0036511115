b014483d25c6d9bf68f147798d5e9b9cd3127f36
tomcpp <tomislav@arch.localhost>
2018-10-23 18:57:30 +0200

    Added file to dz01-additions

---
+++ zon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Zadatak 1
+#Postoje razlike zato što sudo daje naredbi root privilegije pa onda naredba može pristitupiti
+#direktorijima koji bez privilegija nije mogla.
+
+#Zadatak 2
+cd /\!\!
+cd /\-
+
+#Zadatak 3
+#alias update='sudo pacman -Syu'
